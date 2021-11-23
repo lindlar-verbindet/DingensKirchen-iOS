b5f60929c87513278d074a502cb4fcbd8decae32
pixelskull <pascal.schoenthier@th-koeln.de>
2021-11-23 14:56:14 +0100

    Added poll icon to SurveyWidget

--- DingensKirchen/DingensKirchen/View/Widget/SurveyWidget.swift
+++ DingensKirchen/DingensKirchen/View/Widget/SurveyWidget.swift
@@ -14,16 +14,29 @@ struct SurveyWidget: View {
     
     var body: some View {
         VStack(alignment: .leading) {
-            Text(title.uppercased())
-                .font(Font.system(size: 26, weight: .light))
-                .foregroundColor(.black)
-                .padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
-            Text(desc)
-                .font(Font.system(size: 16, weight: .light))
-                .foregroundColor(.black)
-                .padding(EdgeInsets(top: 0, leading: 10, bottom: 10, trailing: 10))
-                .lineLimit(nil)
-                .multilineTextAlignment(.leading)
+            ZStack(alignment: .leading) {
+                HStack {
+                    Spacer()
+                    Image(uiImage: UIImage(named: "ic_poll")!)
+                        .resizable()
+                        .frame(width: 70, height: 70)
+                        .padding(.trailing, 20)
+                        .foregroundColor(.black)
+                        .opacity(0.2)
+                }
+                VStack(alignment: .leading) {
+                    Text(title.uppercased())
+                        .font(Font.system(size: 26, weight: .light))
+                        .foregroundColor(.black)
+                        .padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
+                    Text(desc)
+                        .font(Font.system(size: 16, weight: .light))
+                        .foregroundColor(.black)
+                        .padding(EdgeInsets(top: 0, leading: 10, bottom: 10, trailing: 10))
+                        .lineLimit(nil)
+                        .multilineTextAlignment(.leading)
+                }
+            }
         }
         .widget(background: .tertiaryHighlight)
         .padding(5)
